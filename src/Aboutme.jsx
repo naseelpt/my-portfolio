bf127f13ad99111c447b0bb8ee63d96a9cedc9c5
naseelpt <ptnaseel@gmail.com>
2026-06-03 18:26:44 +0530

    last version

--- src/Aboutme.jsx
+++ src/Aboutme.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import qwe from "./assets/qwer.jpg"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {  faFile  } from '@fortawesome/free-solid-svg-icons'
-import resume from "./assets/MUHAMMED NASEEL PT.pdf"
+import resume from "./assets/MUHAMMED NASEEL PT .pdf"
 
 function Aboutme() {
 
@@ -50,7 +50,7 @@ function Aboutme() {
 
               <div>
                 <h1 className=' text-xl font-bold'>Experience</h1>
-                <p>Wahylab Solutions - Mearn Stack ( 10/2025 - Present ) Kochi </p>
+                <p>Wahylab Solutions - Mearn Stack (08/2025 - 04/2026)Kochi </p>
 
                 <p>Docto Smart - React Js ( 01/2025 - 03/2025 ) Kozhikode </p>
               </div>
